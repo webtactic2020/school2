0ca701efd84e85aa324b7a9d280c632e13b83915
webtactic2020 <kc@webtactic.in>
2020-10-22 17:15:42 +0530

    first commit

---
+++ form/index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	
+	<title></title>
+	<style type="text/css">
+		
+		@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+body {
+  background: url('https://www.freevector.com/uploads/vector/preview/30355/Fluid_Gradient_Background.jpg');
+  background-size:100%;
+  text-align: center;
+ font-family: 'Montserrat', sans-serif;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+#svg_form_time {
+  height: 15px;
+  max-width: 80%;
+  margin: 40px auto 20px;
+  display: block;
+}
+
+#svg_form_time circle,
+#svg_form_time rect {
+  fill: white;
+}
+
+.button {
+  background: rgb(237, 40, 70);
+  border-radius: 100px;
+  padding: 15px 25px;
+  display: inline-block;
+  margin: 10px;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+}
+
+.disabled {
+  display:none;
+}
+
+section {
+  padding: 50px 100px 70px;
+  max-width: 400px;
+  margin: 30px auto;
+  background:white;
+  background:rgba(255,255,255,0.8);
+  backdrop-filter:blur(10px);
+  box-shadow:0px 2px 10px rgba(0,0,0,0.3);
+  border-radius:10px;
+  transition:transform 0.2s ease-in-out;
+}
+
+input {
+  width: 100%;
+  margin: 5px 0px;
+  display: inline-block;
+  padding: 15px 25px;
+  box-sizing: border-box;
+  border-radius: 50px;
+  border: 1px solid lightgrey;
+  font-size: 1em;
+  font-family:inherit;
+  background:white;
+}
+
+p {
+  margin: 5px;
+  text-align: left;
+  font-weight: bold;
+  font-size: 0.9em;
+}
+	</style>
+</head>
+<body>
+<div id="svg_wrap"></div>
+
+<section>
+  <p>Personal information</p>
+  <input type="text" placeholder="Firstname" />
+  <input type="text" placeholder="Surname" />
+  <input type="text" placeholder="Birthdate" />
+  <input type="text" placeholder="Insurance number" />
+  <input type="text" placeholder="Family status" />
+</section>
+
+<section>
+  <p>Address</p>
+  <input type="text" placeholder="Street, nbr" />
+  <input type="text" placeholder="City" />
+  <input type="text" placeholder="Postcode" />
+  <input type="text" placeholder="Country" />
+</section>
+
+<section>
+  <p>Contact information</p>
+  <input type="text" placeholder="Email address" />
+  <input type="text" placeholder="Phone" />
+  <input type="text" placeholder="Mobile" />
+</section>
+
+<section>
+  <p>Application</p>
+  <input type="text" placeholder="Preferred entrance date" />
+  <input type="text" placeholder="Number of people" />
+</section>
+
+<section>
+  <p>General condtitions</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</section>
+
+<div class="button" id="prev">&larr; Previous</div>
+<div class="button" id="next">Next &rarr;</div>
+<div class="button" id="submit">Agree and send application</div>
+</body>
+
+<script type="text/javascript">
+	var base_color = "white";
+var active_color = "rgb(237, 40, 70)";
+
+var child = 1;
+var length = $("section").length - 1;
+$("#prev").addClass("disabled");
+$("#submit").addClass("disabled");
+
+$("section").not("section:nth-of-type(1)").hide();
+$("section").not("section:nth-of-type(1)").css('transform','translateX(100px)');
+
+var svgWidth = length * 200 + 24;
+$("#svg_wrap").html(
+  '<svg version="1.1" id="svg_form_time" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 ' +
+    svgWidth +
+    ' 24" xml:space="preserve"></svg>'
+);
+
+function makeSVG(tag, attrs) {
+  var el = document.createElementNS("http://www.w3.org/2000/svg", tag);
+  for (var k in attrs) el.setAttribute(k, attrs[k]);
+  return el;
+}
+
+for (i = 0; i < length; i++) {
+  var positionX = 12 + i * 200;
+  var rect = makeSVG("rect", { x: positionX, y: 9, width: 200, height: 6 });
+  document.getElementById("svg_form_time").appendChild(rect);
+  // <g><rect x="12" y="9" width="200" height="6"></rect></g>'
+  var circle = makeSVG("circle", {
+    cx: positionX,
+    cy: 12,
+    r: 12,
+    width: positionX,
+    height: 6
+  });
+  document.getElementById("svg_form_time").appendChild(circle);
+}
+
+var circle = makeSVG("circle", {
+  cx: positionX + 200,
+  cy: 12,
+  r: 12,
+  width: positionX,
+  height: 6
+});
+document.getElementById("svg_form_time").appendChild(circle);
+
+$("circle:nth-of-type(1)").css("fill", active_color);
+
+$(".button").click(function () {
+  $("#svg_form_time rect").css("fill", active_color);
+  $("#svg_form_time circle").css("fill", active_color);
+  var id = $(this).attr("id");
+  if (id == "next") {
+    $("#prev").removeClass("disabled");
+    if (child >= length) {
+      $(this).addClass("disabled");
+      $('#submit').removeClass("disabled");
+    }
+    if (child <= length) {
+      child++;
+    }
+  } else if (id == "prev") {
+    $("#next").removeClass("disabled");
+    $('#submit').addClass("disabled");
+    if (child <= 2) {
+      $(this).addClass("disabled");
+    }
+    if (child > 1) {
+      child--;
+    }
+  }
+  var circle_child = child + 1;
+  $("#svg_form_time rect:nth-of-type(n + " + child + ")").css(
+    "fill",
+    base_color
+  );
+  $("#svg_form_time circle:nth-of-type(n + " + circle_child + ")").css(
+    "fill",
+    base_color
+  );
+  var currentSection = $("section:nth-of-type(" + child + ")");
+  currentSection.fadeIn();
+  currentSection.css('transform','translateX(0)');
+ currentSection.prevAll('section').css('transform','translateX(-100px)');
+  currentSection.nextAll('section').css('transform','translateX(100px)');
+  $('section').not(currentSection).hide();
+});
+</script>
+</html>
